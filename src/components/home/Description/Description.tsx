e2057fda59a2b85790679e169e433eea743f8b69
mago_d_Mercurio <davila.feria@gmail.com>
2023-12-16 17:21:27 -0600

    create description.module.sass and placeholder

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,11 +1,27 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+import { PLACEHOLDER_IMAGE } from './placeholder'
+
 
 
 export const Description = () => {
-  
   return (
-    <section>
-      <img src="/images/description.jpeg" alt="products marketplace" />
-    </section>
-    )
+    <section className={styles.Description}>
 
+      <div className={styles.Description__imageContainer}>
+      <Image
+        src="/images/description.jpeg"
+        alt="products marketplace"
+        fill
+        placeholder='blur'
+        blurDataURL={PLACEHOLDER_IMAGE}
+      />
+
+      </div>
+      <div className={styles.Description__text}>
+        <h2>Bring the future today</h2>
+        <p>Future World: Your Gateway to Tomorrow&#39;s Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+      </div>
+    </section>
+  )
 }
