55eb260513f0bb8588590760dc404c89dfc65585
mago_d_Mercurio <davila.feria@gmail.com>
2023-12-16 19:13:18 -0600

    update description.tsx

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,26 +1,40 @@
-import styles from './Description.module.sass';
+"use client"
 import Image from 'next/image';
+import classNames from 'classnames/bind';
+import { useState } from 'react';
+import styles from './Description.module.sass';
 import { PLACEHOLDER_IMAGE } from './placeholder'
 
 
-
 export const Description = () => {
+  const [hasBorder, setBorder] = useState(false);
+
+  const handleClick = () => setBorder(!hasBorder);
+
+  const cx = classNames.bind(styles);
+
+  const buttonStyles = cx('Description__button', {
+    'Description__button--border': hasBorder,
+  });
+
+  console.log(buttonStyles);
+
   return (
     <section className={styles.Description}>
-
-      <div className={styles.Description__imageContainer}>
-      <Image
-        src="/images/description.jpeg"
-        alt="products marketplace"
-        fill
-        placeholder='blur'
-        blurDataURL={PLACEHOLDER_IMAGE}
-      />
-
-      </div>
+      <button onClick={handleClick} className={buttonStyles}>
+        <div className={styles.Description__imageContainer}>
+          <Image
+            src="/images/description.jpeg"
+            alt="products marketplace"
+            fill
+            placeholder='blur'
+            blurDataURL={PLACEHOLDER_IMAGE}
+          />
+        </div>
+      </button>
       <div className={styles.Description__text}>
         <h2>Bring the future today</h2>
-        <p>Future World: Your Gateway to Tomorrow&#39;s Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
+        <p>Future World: Your Gateway to Tomorrow's Tech! Dive into a world of cutting-edge gadgets and gear. Stay ahead of the curve and redefine your digital lifestyle with us.</p>
       </div>
     </section>
   )
